91ea7e482fcf1bab581f6f36875e0ff1bdcb87b9
Alikber_Rashidow <alikberg123@gmail.com>
2024-02-13 18:11:24 +0300

    UPDATE

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 # Section 1
 
 **rcojprcj**
+```python
+
+i = 1111
+a = input()
+if not a == "ENTER":
+    print(i)
+elif a == "ENTER":
+    print("DONE")
+
+```
--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import aiohttp
-
-url = "https://alikberg123.github.io/android-store/"
-r = requests.get(url)
-u = aiohttp.get_payload(url)
-
-print(r.url)
